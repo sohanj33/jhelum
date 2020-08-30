72f9d84c23e22259ea69e513f5d5d6976ddefcc7
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-08-30 20:37:09 +0530

    Update xml_parser.c

--- esb_endpoint/src/xml_parser.c
+++ esb_endpoint/src/xml_parser.c
@@ -13,6 +13,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <libxml/xmlreader.h>
 
 #ifdef LIBXML_READER_ENABLED
@@ -35,73 +36,43 @@ processNode(xmlTextReaderPtr reader) {
 
     value = xmlTextReaderConstValue(reader);
 
-    printf("%d %d %s %d %d", 
-	    xmlTextReaderDepth(reader),
-	    xmlTextReaderNodeType(reader),
-	    name,
-	    xmlTextReaderIsEmptyElement(reader),
-	    xmlTextReaderHasValue(reader));
-    if (value == NULL)
-	printf("\n");
-    else {
-        if (xmlStrlen(value) > 40)
-            printf(" %.40s...\n", value);
-        else
-	    printf(" %s\n", value);
-    }
+    //printf("%s", name);
+    if (value != NULL)
+	{
+	    printf(" %s", value);
+    	}
 }
 
 /**
  * streamFile:
  * @filename: the file name to parse
  *
- * Parse, validate and print information about an XML file.
+ * Parse and print information about an XML file.
  */
 static void
-streamFile(const char *bmd_file_path) {
+streamFile(const char *filename) {
     xmlTextReaderPtr reader;
     int ret;
-    
-    /*
-     * this initialize the library and check potential ABI mismatches
-     * between the version it was compiled for and the actual shared
-     * library used.
-     */
-    
 
-
-    /*
-     * Pass some special parsing options to activate DTD attribute defaulting,
-     * entities substitution and DTD validation
-     */
-    reader = xmlReaderForFile(bmd_file_path, NULL,
-                 XML_PARSE_DTDATTR |  /* default DTD attributes */
-		 XML_PARSE_NOENT |    /* substitute entities */
-		 XML_PARSE_DTDVALID); /* validate with the DTD */
+    reader = xmlReaderForFile(filename, NULL, 0);
     if (reader != NULL) {
         ret = xmlTextReaderRead(reader);
         while (ret == 1) {
             processNode(reader);
             ret = xmlTextReaderRead(reader);
         }
-	/*
-	 * Once the document has been fully parsed check the validation results
-	 */
-	if (xmlTextReaderIsValid(reader) != 1) {
-	    fprintf(stderr, "Document %s does not validate\n", bmd_file_path);
-	}
         xmlFreeTextReader(reader);
         if (ret != 0) {
-            fprintf(stderr, "%s : failed to parse\n", bmd_file_path);
+            fprintf(stderr, "%s : failed to parse\n", filename);
         }
     } else {
-        fprintf(stderr, "Unable to open %s\n",bmd_file_path);
+        fprintf(stderr, "Unable to open %s\n", filename);
     }
 }
 
-
 int processXML(char *bmd_file_path) {
-   
+   bmd_file_path = "/home/utkarsh/Documents/jhelum-master/esb_endpoint/src/bmd.xml";
+   printf("Called processXML with path: %s\n", bmd_file_path);
 
     /*
      * this initialize the library and check potential ABI mismatches
