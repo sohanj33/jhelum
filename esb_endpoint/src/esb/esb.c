4bfd3b2de7a128d588a6df24bc4ac9fe8d0e0543
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-03 18:59:10 +0530

    Database connection added

--- esb_endpoint/src/esb/esb.c
+++ esb_endpoint/src/esb/esb.c
@@ -3,12 +3,13 @@
 #include <bmd_parser.h>
 #include <esb.h>
 #include <stdlib.h>
+
 /**
  * This is the main entry point into the ESB. 
  * It will start processing of a BMD received at the HTTP endpoint.
  */
 int process_esb_request(char* bmd_file_path) {
-    int status = 0; // OK == 0
+    int f_status = 0; // OK == 0
     printf("Handling the BMD %s\n", bmd_file_path);
     /** TODO: 
      * Perform the steps outlined in the Theory of Operation section of
@@ -16,10 +17,15 @@ int process_esb_request(char* bmd_file_path) {
      * a separate module. Suitable unit tests should be created for all
      * the modules, including this one.
      */
+     
+     
     BMD *bmd; 
     bmd=processXML(bmd_file_path);      //To retrieve envelope bmd->bmd_envelope ,For payload use bmd->bmd_payload;
-     
+    
      printf("BMD retrieved\n");        //Weird behaviour --> print statement gets executed only while printing some bmd elements 
-                                      //Add printf("%s",bmd->bmd_envelope->MessageID);
-     return status;
+                                      //Add printf("%s",bmd->bmd_envelope->MessageID);   
+     printf("Loading SQL File...");
+     sqlcon(); 
+     return f_status;
 }
+
