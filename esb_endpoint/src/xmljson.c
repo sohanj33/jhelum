f66c6daf659aff2120038a183e681c82e13419bd
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-04 15:44:18 +0530

    JSON file creation function

---
+++ esb_endpoint/src/xmljson.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void xml2json(char Pay[])
+{
+	char filename[] = "Payload.json";
+        FILE *file;
+    	file = fopen(filename,"w");
+    	
+    	if(file == NULL) 
+    	{
+        	printf("file opening filed");
+        	exit(0);
+    	}
+    	
+    	/* Writes into json file */
+    	fprintf(file,"{\n \"Payload\":\"%s\"\n}",Pay);
+    	
+    	printf("\nPayload json File created\n");
+   	 /* Closes file */
+    	fclose(file);
+    	/* returns json filename */
+    	return strdup(filename);
+}
