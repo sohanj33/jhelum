dd9d58cfa58c97ac8107165ca40cce4b230168a7
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-05 12:24:06 +0530

    Added Filesize check for larger than 5MB

--- esb_endpoint/src/xmljson.c
+++ esb_endpoint/src/xmljson.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 void xml2json(char Pay[])
 {
-	char filename[] = "Payload.json";
+	char filename[] = "/home/utkarsh/Documents/jhelum-master/esb_endpoint/Payload.json";
         FILE *file;
     	file = fopen(filename,"w");
     	
@@ -17,8 +16,28 @@ void xml2json(char Pay[])
     	fprintf(file,"{\n \"Payload\":\"%s\"\n}",Pay);
     	
     	printf("\nPayload json File created\n");
+    
    	 /* Closes file */
     	fclose(file);
+    	
+    	/*File size validation*/
+    	 FILE *fp;
+   	 int size = 0;
+ 
+   	 fp = fopen(filename, "r");
+   	 if (fp == NULL)
+      		  printf("\nFile unable to open ");
+   	 else 
+     	   printf("\nFile opened ");
+   	 fseek(fp, 0, 2);    /* file pointer at the end of file */
+   	 size = ftell(fp);   /* take a position of file pointer un size variable */
+   	
+   	/*Check if Filesize is larger than 5MB*/
+   	if(size>5000000)
+   		printf("\nFile size larger than 5MB not allowed!\n");
+   	  
+   	 fclose(fp); 
+        
     	/* returns json filename */
-    	//return strdup(filename);
+    	return strdup(filename);
 }
