3cbea7726da4de3496a2313fbbc5893206e53498
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-13 11:50:26 +0530

    Update transform.c

--- esb_endpoint/src/esb/transform.c
+++ esb_endpoint/src/esb/transform.c
@@ -2,15 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-<<<<<<< HEAD
-=======
-void check_transform(char type[], int route_id)
+void check_transform(char type[], int route_id, char* transport_key)
 {
-	if(!strcmp(type,"json"))
+	if(!strcmp(type,"Json_transform"))
 	{
 		printf("\nNeed transformation\n");
-		no_transform(route_id);
-		//add route_id
+		Json_transform(route_id, transport_key);
 	}
 	else
 	{
@@ -19,9 +16,8 @@ void check_transform(char type[], int route_id)
 	}
 	
 }
->>>>>>> 387e8cc560d9d7b4e7ab7a9de91a0f06ffa7fc9e
 
-void no_transform(int route_id)
+void Json_transform(int route_id, char* transport_key)
 {
 	char temp[100];
 	FILE *fp = fopen("Payload.json", "r");
@@ -37,7 +33,7 @@ void no_transform(int route_id)
 	
 	char payload[strlen(temp)];
 	int k=0;
-	for(i=0;i<strlen(temp);i++)
+	for(int i=0;i<strlen(temp);i++)
 	{
 		if(temp[i]==':')
 		{
@@ -53,30 +49,6 @@ void no_transform(int route_id)
 	
 	printf("\nPayload:\t%s\n",payload);
 	
-<<<<<<< HEAD
-	//add_payload(payload);
-	
-}
-
-
-
-
-
-
-void check_transform(char type[])
-{
-	if(!strcmp(type,"json"))
-	{
-		printf("\nNeed transformation\n");
-		no_transform();
-	}
-	else
-	{
-		printf("\nNo transformation needed\n");
-		
-	}
-=======
-	add_payload(payload, route_id);
->>>>>>> 387e8cc560d9d7b4e7ab7a9de91a0f06ffa7fc9e
+	add_payload(payload, route_id, transport_key);
 	
 }
