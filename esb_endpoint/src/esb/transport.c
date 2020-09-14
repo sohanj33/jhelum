3619ba66a86ac20a8a7476af2d26f1b8ce6d09cf
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-14 12:32:10 +0530

    Added email transport service
    The email transport requires filename to be entered

--- esb_endpoint/src/esb/transport.c
+++ esb_endpoint/src/esb/transport.c
@@ -9,6 +9,18 @@ Apply_transport_service(char URL[], char transport_service[])
 		printf("\nHTTP service applied\n");
 		send_http_request(URL);
 	}
+	else if(!strcmp(transport_service,"email"))
+	{
+		char filename[100];
+		printf("Enter file to read:\t");
+		scanf("%s",&filename);
+		printf("\nemail service applied\n");
+		int mail_status = send_mail(URL, filename);
+		if(mail_status==0)
+			printf("Mail sent\n");
+		else
+			printf("Failed to send the Mail\n");
+	}
 	else
 	{
 		printf("\nNo service found\n");
