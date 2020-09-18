753731f2db653274018866cc9ec7f8fc77b5acd4
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-18 12:33:00 +0530

    Update email.c

--- esb_endpoint/src/adapter/email.c
+++ esb_endpoint/src/adapter/email.c
@@ -1,5 +1,3 @@
-/* gcc emailtest.c -lcurl */
-
 #include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
@@ -9,43 +7,45 @@
 #define TO "jhelumnho2020@gmail.com"
 #define CC "jhelumnho2020@gmail.com"
 
-struct upload_status
-{
+struct upload_status {
     int lines_read;
 };
 
-static const char *payload_file_path[] = {
+static
+const char * payload_file_path[] = {
     "Date: Mon, 29 Nov 2010 21:54:29 +1100\r\n",
-    "To: " TO "\r\n",
-    "From: " FROM " (Example User)\r\n",
-    "Cc: " CC " (Another example )\r\n",
+    "To: "
+    TO "\r\n",
+    "From: "
+    FROM " (Example User)\r\n",
+    "Cc: "
+    CC " (Another example )\r\n",
     "Message-ID: <dc7cb-11db-487a-9f3a-e52a9458ef@"
     "rfcpedant.example.org>\r\n",
     "Subject: SMTP TLS example message\r\n",
-    "\r\n", /* empty line to divide headers from body, see RFC5322 */
+    "\r\n",
+    /* empty line to divide headers from body, see RFC5322 */
     "The body of the message starts here.\r\n",
     "\r\n",
     "It could be a lot of lines, could be MIME encoded, whatever.\r\n",
     "Check RFC5322.\r\n",
-    NULL};
+    NULL
+};
 
-static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp)
-{
-    struct upload_status *upload_ctx = (struct upload_status *)userp;
-    const char *data;
+static size_t payload_source(void * ptr, size_t size, size_t nmemb, void * userp) {
+    struct upload_status * upload_ctx = (struct upload_status * ) userp;
+    const char * data;
 
-    if ((size == 0) || (nmemb == 0) || ((size * nmemb) < 1))
-    {
+    if ((size == 0) || (nmemb == 0) || ((size * nmemb) < 1)) {
         return 0;
     }
 
-    data = payload_file_path[upload_ctx->lines_read];
+    data = payload_file_path[upload_ctx -> lines_read];
 
-    if (data)
-    {
+    if (data) {
         size_t len = strlen(data);
         memcpy(ptr, data, len);
-        upload_ctx->lines_read++;
+        upload_ctx -> lines_read++;
 
         return len;
     }
@@ -53,20 +53,18 @@ static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp)
     return 0;
 }
 
-int send_mail(char *to, char *file_path)
-{
+int send_mail(char * to, char * file_path) {
     printf("Sending mail to %s\n", to);
 
-    CURL *curl;
+    CURL * curl;
     CURLcode res = CURLE_OK;
-    struct curl_slist *recipients = NULL;
+    struct curl_slist * recipients = NULL;
     struct upload_status upload_ctx;
 
     upload_ctx.lines_read = 0;
 
     curl = curl_easy_init();
-    if (curl)
-    {
+    if (curl) {
         /* Set username and password */
         curl_easy_setopt(curl, CURLOPT_USERNAME, "jhelumnho2020");
         curl_easy_setopt(curl, CURLOPT_PASSWORD, "jhelum@Nho2020");
@@ -94,20 +92,18 @@ int send_mail(char *to, char *file_path)
          * body of the message). You could just use the CURLOPT_READDATA option to
          * specify a FILE pointer to read from. */
         //curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
-        FILE *fp = fopen(file_path, "r");
+        FILE * fp = fopen(file_path, "r");
         curl_easy_setopt(curl, CURLOPT_READDATA, fp);
-        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-
-      
+        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1 L);
 
         /* Send the message */
-        /* res = 0 means everything is ok */
+        /* res =0 means everything is ok */
         res = curl_easy_perform(curl);
 
         /* Check for errors */
         if (res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
+                curl_easy_strerror(res));
 
         /* Free the list of recipients */
         curl_slist_free_all(recipients);
@@ -116,5 +112,5 @@ int send_mail(char *to, char *file_path)
         curl_easy_cleanup(curl);
     }
 
-    return (int)res;
+    return (int) res;
 }
