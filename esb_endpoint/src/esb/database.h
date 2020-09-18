acfeea8704ab904251c5578507059487e4befd58
utkarshgupta98 <31436503+utkarshgupta98@users.noreply.github.com>
2020-09-18 12:50:33 +0530

    Update database.h

--- esb_endpoint/src/esb/database.h
+++ esb_endpoint/src/esb/database.h
@@ -1,14 +1,14 @@
-#ifndef DATABASE 
-#define DATABASE 
+#ifndef DATABASE
+#define DATABASE
 
-#define INSERT_IN_ESB_REQUEST  "INSERT INTO esb_request(sender_id,dest_id,message_type,reference_id,message_id,received_on,status) VALUES ('%s','%s','%s','%s','%s','%s','%s')"
+#define INSERT_IN_ESB_REQUEST "INSERT INTO esb_request(sender_id,dest_id,message_type,reference_id,message_id,received_on,status) VALUES ('%s','%s','%s','%s','%s','%s','%s')"
 #define SELECT_ACTIVE_ROUTE "SELECT route_id FROM routes WHERE sender = '%s' AND destination = '%s' AND message_type= '%s'  AND is_active=b'1'"
 #define SELECT_TRANSPORT_CONFIG "SELECT * FROM transport_config WHERE route_id='%d'"
 #define SELECT_TRANSFORM_CONFIG "SELECT * FROM transform_config WHERE route_id='%d'"
 
-#define CHECK_NEW_REQUEST "SELECT * FROM esb_request WHERE status = 'available' AND id='%d' LIMIT 1" 
+#define CHECK_NEW_REQUEST "SELECT * FROM esb_request WHERE status = 'available' AND id='%d' LIMIT 1"
 
-#define AVAILABLE_TO_TAKEN "UPDATE esb_request SET status = 'taken'  WHERE id='%d'"  
+#define AVAILABLE_TO_TAKEN "UPDATE esb_request SET status = 'taken'  WHERE id='%d'"
 #define TAKEN_TO_DONE "UPDATE esb_request SET status = 'done'  WHERE id='%d'"
 #define STATUS_TO_ERROR "UPDATE esb_request SET status = 'error'  WHERE id='%d'"
 
@@ -23,8 +23,8 @@
 #define database "esb_db"
 #define port 3306
 
-int insert_in_esb_request(BMD *bmd);
-int select_active_route(const unsigned char *Sender, const unsigned char *Destination, const unsigned char *MessageType);
+int insert_in_esb_request(BMD * bmd);
+int select_active_route(const unsigned char * Sender, const unsigned char * Destination, const unsigned char * MessageType);
 int select_transport_config(int route_id);
 int select_transform_config(int route_id);
 
