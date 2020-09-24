3994b4e388894c8b54784814b1de7b5f0250a168
1ss2 <ssrivastava1099@gmail.com>
2020-09-24 10:02:25 +0530

    change

---
+++ esb_endpoint/src/database/database.h
@@ -0,0 +1,31 @@
+#ifndef DATABASE
+#define DATABASE
+
+#define INSERT_IN_ESB_REQUEST "INSERT INTO esb_request(sender_id,dest_id,message_type,reference_id,message_id,received_on,status) VALUES ('%s','%s','%s','%s','%s','%s','%s')"
+#define SELECT_ACTIVE_ROUTE "SELECT route_id FROM routes WHERE sender = '%s' AND destination = '%s' AND message_type= '%s'  AND is_active=b'1'"
+#define SELECT_TRANSPORT_CONFIG "SELECT * FROM transport_config WHERE route_id='%d'"
+#define SELECT_TRANSFORM_CONFIG "SELECT * FROM transform_config WHERE route_id='%d'"
+
+#define CHECK_NEW_REQUEST "SELECT * FROM esb_request WHERE status = 'available' AND id='%d' LIMIT 1"
+
+#define AVAILABLE_TO_TAKEN "UPDATE esb_request SET status = 'taken'  WHERE id='%d'"
+#define TAKEN_TO_DONE "UPDATE esb_request SET status = 'done'  WHERE id='%d'"
+#define STATUS_TO_ERROR "UPDATE esb_request SET status = 'error'  WHERE id='%d'"
+
+#define GET_routeid "SELECT route_id FROM routes WHERE sender = '%s' AND destination = '%s' AND message_type= '%s'"
+#define GET_transformkey "SELECT config_key FROM transform_config WHERE route_id='%d'"
+#define GET_transportkey "SELECT config_key FROM transport_config WHERE route_id='%d'"
+#define GET_transportvalue "SELECT config_value FROM transport_config WHERE route_id='%d'"
+
+#define server "localhost"
+#define user "splunk_user"
+#define password "password"
+#define database "esb_db"
+#define port 3306
+
+int insert_in_esb_request(BMD * bmd);
+int select_active_route(const unsigned char * Sender, const unsigned char * Destination, const unsigned char * MessageType);
+int select_transport_config(int route_id);
+int select_transform_config(int route_id);
+
+#endif
---
+++ esb_endpoint/src/route/transport.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Apply transport service */
+int Apply_transport_service(char URL[], char transport_service[], char SENDER[], char transform_key[]) {
+    int status = 1;
+    printf("entered into apply_transport_service");
+    if (!strcmp(transport_service, "HTTP")) {
+        printf("\nHTTP service applied\n");
+        send_http_request(URL);
+        return status;
+    } else if (!strcmp(transport_service, "email")){ 
+    	if(!strcmp(transform_key,"Json_transform")){
+        char filename[100];
+        strcpy(filename, SENDER);
+    	strcat(filename, "Payload.json");
+        printf("Enter file to read:\t");
+        //scanf("%s", & filename);
+        printf("\nemail service applied\n");
+        int mail_status = send_mail(URL, filename);
+        if (mail_status == 0) {
+            printf("Mail sent\n");
+            return status;
+        } else {
+            printf("Failed to send the Mail\n");
+            status = 0;
+        }
+     } 
+     	else if (! strcmp(transform_key,"Xml_transform")) {
+        char filename[100];
+        strcpy(filename, SENDER);
+    	strcat(filename, "Payload.xml");
+        printf("Enter file to read:\t");
+        //scanf("%s", & filename);
+        printf("\nemail service applied\n");
+        int mail_status = send_mail(URL, filename);
+        if (mail_status == 0) {
+            printf("Mail sent\n");
+            return status;
+        } else {
+            printf("Failed to send the Mail\n");
+            status = 0;
+        }
+     }
+    } else {
+        status = 0;
+        printf("\nNo service found\n");
+    }
+    return status;
+
+}
