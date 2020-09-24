a264dd0049f2181026810a4551348451d79bc353
1ss2 <ssrivastava1099@gmail.com>
2020-09-24 18:29:12 +0530

    sql script

--- esb_endpoint/scripts/SQLscript.sql
+++ esb_endpoint/scripts/SQLscript.sql
@@ -110,33 +110,28 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 INSERT INTO routes
 VALUES (1,"756E2EAA-1D5B-4BC0-ACC4-4CEB669408DA","6393F82F-4687-433D-AA23-1966330381FE","CreditReport",0x01),
 (2,"556E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","6323D82F-4687-433D-AA23-1966330381FE","DebitReport",0x01),   
-(3,"666E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","8323D82F-4687-433D-AA23-1966330381FE", "DebitReport",0x01);        
+(3,"666E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","8323D82F-4687-433D-AA23-1966330381FE", "DebitReport",0x01),
+(4,"776E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","6323D82F-4687-433D-AA23-1966330381FE","DebitReport",0x01),   
+(5,"886E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","8323D82F-4687-433D-AA23-1966330381FE", "DebitReport",0x01),
+(6,"076E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","6323D82F-4687-433D-AA23-1966330381FE","DebitReport",0x01),   
+(7,"086E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","8323D82F-4687-433D-AA23-1966330381FE", "DebitReport",0x01);           
 
 
 INSERT INTO transform_config
 VALUES (1001,1,"Json_transform","json"),
 (1002,2,"Json_transform","json"),
-(1003,3,"Json_transform","json");
+(1003,3,"Json_transform","json"),
+(1004,4,"Xml_transform","xml"),
+(1005,5,"Xml_transform","xml"),
+(1006,6,"Xml_transform","xml"),
+(1007,7,"Json_transform","json")
+;
 
 INSERT INTO transport_config
 VALUES (2001,1,"https://ifsc.razorpay.com/","HTTP"),
 (2002,2,"testraavi1@gmail.com","email"),
-(2003,3,"https://ifsc.razorpay.com/","HTTP");
-
-
-
-
-INSERT INTO routes
-VALUES 
-(4,"776E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","6323D82F-4687-433D-AA23-1966330381FE","DebitReport",0x01),   
-(5,"886E2EAA-1D5B-5BC0-BCC4-4CEB669408DA","8323D82F-4687-433D-AA23-1966330381FE", "DebitReport",0x01);        
-
-
-INSERT INTO transform_config
-VALUES (1004,4,"Xml_transform","xml"),
-(1005,5,"Xml_transform","xml")
-;
-
-INSERT INTO transport_config
-VALUES (2004,4,"testraavi1@gmail.com","email"),
-(2005,5,"https://ifsc.razorpay.com/","HTTP");
+(2003,3,"https://ifsc.razorpay.com/","HTTP"),
+(2004,4,"testraavi1@gmail.com","email"),
+(2005,5,"https://ifsc.razorpay.com/","HTTP"),
+(2006,6,"raavi","SFTP"),
+(2007,7,"raavi","SFTP");
